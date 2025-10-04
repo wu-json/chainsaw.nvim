3a683512d3d51b2d4bd2a432aac7ce55d9197eb0
wu-json <jason.wu2153@gmail.com>
2025-10-04 13:08:02 -0700

    feat: Normal

--- lua/lush_theme/reze.lua
+++ lua/lush_theme/reze.lua
@@ -105,7 +105,7 @@ local theme = lush(function(injected_functions)
 		-- MsgSeparator   { }, -- Separator for scrolled messages, `msgsep` flag of 'display'
 		-- MoreMsg        { }, -- |more-prompt|
 		-- NonText        { }, -- '@' at the end of the window, characters from 'showbreak' and other characters that do not really exist in the text (e.g., ">" displayed when a double-wide character doesn't fit at the end of the line). See also |hl-EndOfBuffer|.
-		Normal({ bg = "NONE" }), -- Normal text
+		Normal({ fg = reze_colors.white, bg = "NONE" }), -- Normal text
 		-- NormalFloat    { }, -- Normal text in floating windows.
 		-- FloatBorder    { }, -- Border of floating windows.
 		-- FloatTitle     { }, -- Title of floating windows.
