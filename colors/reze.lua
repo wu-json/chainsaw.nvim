db898cfd016b0e3010acad3369fbbc90db06e873
wu-json <jason.wu2153@gmail.com>
2025-10-04 12:47:04 -0700

    feat: update

--- colors/reze.lua
+++ colors/reze.lua
@@ -1,6 +1,6 @@
 -- You probably always want to set this in your vim file
 vim.opt.background = "dark"
-vim.g.colors_name = "lush_template"
+vim.g.colors_name = "reze"
 
 -- By setting our module to nil, we clear lua's cache,
 -- which means the require ahead will *always* occur.
