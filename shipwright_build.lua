642d7ab88a1634acbb7aa187461d094f89a81625
wu-json <jason.wu2153@gmail.com>
2025-10-04 23:02:57 -0700

    feat: build

--- shipwright_build.lua
+++ shipwright_build.lua
@@ -1,10 +1,10 @@
 local lushwright = require("shipwright.transform.lush")
 run(
-	require("my.lush.colorscheme"),
+	require("lush_theme.reze"),
 	-- generate lua code
 	lushwright.to_lua,
 	-- write the lua code into our destination.
 	-- you must specify open and close markers yourself to account
 	-- for differing comment styles, patchwrite isn't limited to lua files.
-	{ patchwrite, "colors/colorscheme.lua", "-- PATCH_OPEN", "-- PATCH_CLOSE" }
+	{ patchwrite, "reze.lua", "-- PATCH_OPEN", "-- PATCH_CLOSE" }
 )
