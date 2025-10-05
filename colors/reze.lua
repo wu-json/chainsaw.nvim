3818900c317fc32293debf6b67c2430df70a7456
wu-json <jason.wu2153@gmail.com>
2025-10-05 10:45:01 -0700

    feat: cleanup comments

--- colors/reze.lua
+++ colors/reze.lua
@@ -43,12 +43,10 @@ local colors = {
 	-- PATCH_CLOSE
 }
 
--- colorschemes generally want to do this
 vim.cmd("highlight clear")
 vim.cmd("set t_Co=256")
 vim.cmd("let g:colors_name='reze'")
 
--- apply highlight groups
 for group, attrs in pairs(colors) do
 	vim.api.nvim_set_hl(0, group, attrs)
 end
