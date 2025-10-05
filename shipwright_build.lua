32adbc02c9bebf3432dfb364bd75ff4abeeafefe
wu-json <jason.wu2153@gmail.com>
2025-10-05 10:11:29 -0700

    fix: build

--- shipwright_build.lua
+++ shipwright_build.lua
@@ -1,3 +1,3 @@
 local lushwright = require("shipwright.transform.lush")
 
-run(require("themes.reze"), lushwright.to_lua, { patchwrite, "colors/reze.lua", "-- PATCH_OPEN", "-- PATCH_CLOSE" })
+run(require("lush_theme.reze"), lushwright.to_lua, { patchwrite, "colors/reze.lua", "-- PATCH_OPEN", "-- PATCH_CLOSE" })
