18a5d514fcf45ed7831495c971e510f2eaf0d355
Jason Wu <45532884+wu-json@users.noreply.github.com>
2025-10-07 02:25:36 -0400

    feat(theme): add aki (#1)
    * feat: template
    
    * feat: palette
    
    * feat: organize colors
    
    * feat: aki

---
+++ lua/lush_theme/aki.lua
@@ -0,0 +1,270 @@
+local lush = require("lush")
+local hsl = lush.hsl
+
+local palette = {
+	blue = hsl(213, 57, 60),
+	blue_light = hsl(190, 48, 65),
+	blue_washed = hsl(190, 48, 85),
+	white = hsl(340, 8, 97),
+	black = hsl(0, 0, 10),
+	gray = hsl(126, 6, 60),
+	gray_muted = hsl(0, 0, 40),
+}
+
+-- LSP/Linters mistakenly show `undefined global` errors in the spec, they may
+-- support an annotation like the following. Consult your server documentation.
+---@diagnostic disable: undefined-global
+local theme = lush(function(injected_functions)
+	local sym = injected_functions.sym
+	return {
+		-- The following are the Neovim (as of 0.8.0-dev+100-g371dfb174) highlight
+		-- groups, mostly used for styling UI elements.
+		-- Comment them out and add your own properties to override the defaults.
+		-- An empty definition `{}` will clear all styling, leaving elements looking
+		-- like the 'Normal' group.
+		-- To be able to link to a group, it must already be defined, so you may have
+		-- to reorder items as you go.
+		--
+		-- See :h highlight-groups
+		--
+		-- ColorColumn    { }, -- Columns set with 'colorcolumn'
+		-- Conceal        { }, -- Placeholder characters substituted for concealed text (see 'conceallevel')
+		-- Cursor         { }, -- Character under the cursor
+		-- CurSearch      { }, -- Highlighting a search pattern under the cursor (see 'hlsearch')
+		-- lCursor        { }, -- Character under the cursor when |language-mapping| is used (see 'guicursor')
+		-- CursorIM       { }, -- Like Cursor, but used when in IME mode |CursorIM|
+		-- CursorColumn   { }, -- Screen-column at the cursor, when 'cursorcolumn' is set.
+		-- CursorLine     { }, -- Screen-line at the cursor, when 'cursorline' is set. Low-priority if foreground (ctermfg OR guifg) is not set.
+		Directory({ fg = palette.blue }), -- Directory names (and other special names in listings)
+		-- DiffAdd        { }, -- Diff mode: Added line |diff.txt|
+		-- DiffChange     { }, -- Diff mode: Changed line |diff.txt|
+		-- DiffDelete     { }, -- Diff mode: Deleted line |diff.txt|
+		-- DiffText       { }, -- Diff mode: Changed text within a changed line |diff.txt|
+		-- EndOfBuffer    { }, -- Filler lines (~) after the end of the buffer. By default, this is highlighted like |hl-NonText|.
+		-- TermCursor     { }, -- Cursor in a focused terminal
+		-- TermCursorNC   { }, -- Cursor in an unfocused terminal
+		-- ErrorMsg       { }, -- Error messages on the command line
+		-- VertSplit      { }, -- Column separating vertically split windows
+		-- Folded         { }, -- Line used for closed folds
+		-- FoldColumn     { }, -- 'foldcolumn'
+		-- SignColumn     { }, -- Column where |signs| are displayed
+		-- IncSearch      { }, -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
+		-- Substitute     { }, -- |:substitute| replacement text highlighting
+		-- LineNr         { }, -- Line number for ":number" and ":#" commands, and when 'number' or 'relativenumber' option is set.
+		-- LineNrAbove    { }, -- Line number for when the 'relativenumber' option is set, above the cursor line
+		-- LineNrBelow    { }, -- Line number for when the 'relativenumber' option is set, below the cursor line
+		-- CursorLineNr   { }, -- Like LineNr when 'cursorline' or 'relativenumber' is set for the cursor line.
+		-- CursorLineFold { }, -- Like FoldColumn when 'cursorline' is set for the cursor line
+		-- CursorLineSign { }, -- Like SignColumn when 'cursorline' is set for the cursor line
+		-- MatchParen     { }, -- Character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
+		ModeMsg({ fg = palette.blue_light }), -- 'showmode' message (e.g., "-- INSERT -- ")
+		-- MsgArea        { }, -- Area for messages and cmdline
+		-- MsgSeparator   { }, -- Separator for scrolled messages, `msgsep` flag of 'display'
+		MoreMsg({ fg = palette.blue }), -- |more-prompt|
+		-- NonText        { }, -- '@' at the end of the window, characters from 'showbreak' and other characters that do not really exist in the text (e.g., ">" displayed when a double-wide character doesn't fit at the end of the line). See also |hl-EndOfBuffer|.
+		Normal({ fg = palette.white, bg = "NONE" }), -- Normal text
+		NormalFloat({ bg = "NONE" }), -- Normal text in floating windows.
+		FloatBorder({ fg = palette.gray_muted }), -- Border of floating windows.
+		-- FloatTitle     { }, -- Title of floating windows.
+		-- NormalNC       { }, -- normal text in non-current windows
+		-- Pmenu          { }, -- Popup menu: Normal item.
+		-- PmenuSel       { }, -- Popup menu: Selected item.
+		-- PmenuKind      { }, -- Popup menu: Normal item "kind"
+		-- PmenuKindSel   { }, -- Popup menu: Selected item "kind"
+		-- PmenuExtra     { }, -- Popup menu: Normal item "extra text"
+		-- PmenuExtraSel  { }, -- Popup menu: Selected item "extra text"
+		-- PmenuSbar      { }, -- Popup menu: Scrollbar.
+		-- PmenuThumb     { }, -- Popup menu: Thumb of the scrollbar.
+		Question({ fg = palette.blue }), -- |hit-enter| prompt and yes/no questions
+		QuickFixLine({ fg = palette.blue }), -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
+		-- Search         { }, -- Last search pattern highlighting (see 'hlsearch'). Also used for similar items that need to stand out.
+		-- SpecialKey     { }, -- Unprintable characters: text displayed differently from what it really is. But not 'listchars' whitespace. |hl-Whitespace|
+		-- SpellBad       { }, -- Word that is not recognized by the spellchecker. |spell| Combined with the highlighting used otherwise.
+		-- SpellCap       { }, -- Word that should start with a capital. |spell| Combined with the highlighting used otherwise.
+		-- SpellLocal     { }, -- Word that is recognized by the spellchecker as one that is used in another region. |spell| Combined with the highlighting used otherwise.
+		-- SpellRare      { }, -- Word that is recognized by the spellchecker as one that is hardly ever used. |spell| Combined with the highlighting used otherwise.
+		StatusLine({ bg = palette.black }), -- Status line of current window
+		-- StatusLineNC   { }, -- Status lines of not-current windows. Note: If this is equal to "StatusLine" Vim will use "^^^" in the status line of the current window.
+		TabLine({ fg = palette.white, bg = "NONE" }), -- Tab pages line, not active tab page label
+		TabLineFill({ bg = "NONE" }), -- Tab pages line, where there are no labels (transparent)
+		TabLineSel({ fg = palette.blue, bg = "NONE", gui = "bold" }), -- Tab pages line, active tab page label
+		-- Title          { }, -- Titles for output from ":set all", ":autocmd" etc.
+		-- Visual         { }, -- Visual mode selection
+		-- VisualNOS      { }, -- Visual mode selection when vim is "Not Owning the Selection".
+		-- WarningMsg     { }, -- Warning messages
+		-- Whitespace     { }, -- "nbsp", "space", "tab" and "trail" in 'listchars'
+		Winseparator({ fg = palette.black }), -- Separator between window splits. Inherts from |hl-VertSplit| by default, which it will replace eventually.
+		-- WildMenu       { }, -- Current match in 'wildmenu' completion
+		-- WinBar         { }, -- Window bar of current window
+		-- WinBarNC       { }, -- Window bar of not-current windows
+
+		-- Common vim syntax groups used for all kinds of code and markup.
+		-- Commented-out groups should chain up to their preferred (*) group
+		-- by default.
+		--
+		-- See :h group-name
+		--
+		-- Uncomment and edit if you want more specific syntax highlighting.
+
+		-- Comment        { }, -- Any comment
+
+		-- Constant       { }, -- (*) Any constant
+		String({ fg = palette.blue_light }), --   A string constant: "this is a string"
+		-- Character      { }, --   A character constant: 'c', '\n'
+		-- Number         { }, --   A number constant: 234, 0xff
+		-- Boolean        { }, --   A boolean constant: TRUE, false
+		-- Float          { }, --   A floating point constant: 2.3e10
+
+		Identifier({ fg = palette.blue }), -- (*) Any variable name
+		Function({ fg = palette.blue }), --   Function name (also: methods for classes)
+
+		-- Statement      { }, -- (*) Any statement
+		-- Conditional    { }, --   if, then, else, endif, switch, etc.
+		-- Repeat         { }, --   for, do, while, etc.
+		-- Label          { }, --   case, default, etc.
+		-- Operator       { }, --   "sizeof", "+", "*", etc.
+		Keyword({ fg = palette.gray, gui = "bold" }), --   any other keyword (async, await, etc.)
+		-- Exception      { }, --   try, catch, throw
+
+		-- PreProc        { }, -- (*) Generic Preprocessor
+		-- Include        { }, --   Preprocessor #include
+		-- Define         { }, --   Preprocessor #define
+		-- Macro          { }, --   Same as Define
+		-- PreCondit      { }, --   Preprocessor #if, #else, #endif, etc.
+
+		-- Type           { }, -- (*) int, long, char, etc.
+		StorageClass({ fg = palette.gray, gui = "bold" }), --   static, register, volatile, etc. (const, let, var)
+		-- Structure      { }, --   struct, union, enum, etc.
+		-- Typedef        { }, --   A typedef
+
+		Special({ fg = palette.blue }), -- (*) Any special symbol
+		-- SpecialChar({ fg = palette.blue }), --   Special character in a constant
+		-- Tag({ fg = palette.blue }), --   You can use CTRL-] on this
+		-- Delimiter      { }, --   Character that needs attention
+		-- SpecialComment({ fg = palette.blue }), --   Special things inside a comment (e.g. '\n')
+		-- Debug({ fg = palette.blue }), --   Debugging statements
+
+		-- Underlined     { gui = "underline" }, -- Text that stands out, HTML links
+		-- Ignore         { }, -- Left blank, hidden |hl-Ignore| (NOTE: May be invisible here in template)
+		-- Error          { }, -- Any erroneous construct
+		-- Todo           { }, -- Anything that needs extra attention; mostly the keywords TODO FIXME and XXX
+
+		-- These groups are for the native LSP client and diagnostic system. Some
+		-- other LSP clients may use these groups, or use their own. Consult your
+		-- LSP client's documentation.
+
+		-- See :h lsp-highlight, some groups may not be listed, submit a PR fix to lush-template!
+		--
+		-- LspReferenceText            { } , -- Used for highlighting "text" references
+		-- LspReferenceRead            { } , -- Used for highlighting "read" references
+		-- LspReferenceWrite           { } , -- Used for highlighting "write" references
+		-- LspCodeLens                 { } , -- Used to color the virtual text of the codelens. See |nvim_buf_set_extmark()|.
+		-- LspCodeLensSeparator        { } , -- Used to color the seperator between two or more code lens.
+		-- LspSignatureActiveParameter { } , -- Used to highlight the active parameter in the signature help. See |vim.lsp.handlers.signature_help()|.
+
+		-- See :h diagnostic-highlights, some groups may not be listed, submit a PR fix to lush-template!
+
+		-- Neo-tree highlights (LazyVim default file manager)
+		-- NeoTreeDirectoryIcon({ fg = palette.blue }),
+		-- NeoTreeDirectoryName({ fg = palette.blue }),
+		NeoTreeGitModified({ fg = palette.blue_washed }),
+		-- NeoTreeGitUntracked({  }),
+		NeoTreeGitIgnored({ fg = palette.gray }),
+		NeoTreeGitStaged({ fg = palette.blue_washed }),
+		-- NeoTreeGitUnstaged({  }),
+		-- NeoTreeGitConflict({  }),
+		--
+		-- DiagnosticError            { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+		-- DiagnosticWarn             { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+		DiagnosticInfo({ fg = palette.blue }), -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+		DiagnosticHint({ fg = palette.blue }), -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+		DiagnosticOk({ fg = palette.blue_light }), -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+		-- DiagnosticVirtualTextError { } , -- Used for "Error" diagnostic virtual text.
+		-- DiagnosticVirtualTextWarn  { } , -- Used for "Warn" diagnostic virtual text.
+		DiagnosticVirtualTextInfo({ fg = palette.blue }), -- Used for "Info" diagnostic virtual text.
+		DiagnosticVirtualTextHint({ fg = palette.blue }), -- Used for "Hint" diagnostic virtual text.
+		DiagnosticVirtualTextOk({ fg = palette.blue_light }), -- Used for "Ok" diagnostic virtual text.
+		-- DiagnosticUnderlineError   { } , -- Used to underline "Error" diagnostics.
+		-- DiagnosticUnderlineWarn    { } , -- Used to underline "Warn" diagnostics.
+		-- DiagnosticUnderlineInfo    { } , -- Used to underline "Info" diagnostics.
+		-- DiagnosticUnderlineHint    { } , -- Used to underline "Hint" diagnostics.
+		-- DiagnosticUnderlineOk      { } , -- Used to underline "Ok" diagnostics.
+		-- DiagnosticFloatingError    { } , -- Used to color "Error" diagnostic messages in diagnostics float. See |vim.diagnostic.open_float()|
+		-- DiagnosticFloatingWarn     { } , -- Used to color "Warn" diagnostic messages in diagnostics float.
+		DiagnosticFloatingInfo({ fg = palette.blue }), -- Used to color "Info" diagnostic messages in diagnostics float.
+		DiagnosticFloatingHint({ fg = palette.blue }), -- Used to color "Hint" diagnostic messages in diagnostics float.
+		DiagnosticFloatingOk({ fg = palette.blue_light }), -- Used to color "Ok" diagnostic messages in diagnostics float.
+		-- DiagnosticSignError        { } , -- Used for "Error" signs in sign column.
+		-- DiagnosticSignWarn         { } , -- Used for "Warn" signs in sign column.
+		DiagnosticSignInfo({ fg = palette.blue }), -- Used for "Info" signs in sign column.
+		DiagnosticSignHint({ fg = palette.blue }), -- Used for "Hint" signs in sign column.
+		DiagnosticSignOk({ fg = palette.blue_light }), -- Used for "Ok" signs in sign column.
+
+		-- Tree-Sitter syntax groups.
+		--
+		-- See :h treesitter-highlight-groups, some groups may not be listed,
+		-- submit a PR fix to lush-template!
+		--
+		-- Tree-Sitter groups are defined with an "@" symbol, which must be
+		-- specially handled to be valid lua code, we do this via the special
+		-- sym function. The following are all valid ways to call the sym function,
+		-- for more details see https://www.lua.org/pil/5.html
+		--
+		-- sym("@text.literal")
+		-- sym('@text.literal')
+		-- sym"@text.literal"
+		-- sym'@text.literal'
+		--
+		-- For more information see https://github.com/rktjmp/lush.nvim/issues/109
+
+		-- sym"@text.literal"      { }, -- Comment
+		-- sym"@text.reference"    { }, -- Identifier
+		-- sym"@text.title"        { }, -- Title
+		-- sym"@text.uri"          { }, -- Underlined
+		-- sym"@text.underline"    { }, -- Underlined
+		-- sym"@text.todo"         { }, -- Todo
+		-- sym"@comment"           { }, -- Comment
+		-- sym"@punctuation"       { }, -- Delimiter
+		-- sym"@constant"          { }, -- Constant
+		-- sym"@constant.builtin"  { }, -- Special
+		-- sym"@constant.macro"    { }, -- Define
+		-- sym"@define"            { }, -- Define
+		-- sym"@macro"             { }, -- Macro
+		sym("@string")({ fg = palette.blue_light }), -- String
+		-- sym"@string.escape"     { }, -- SpecialChar
+		-- sym"@string.special"    { }, -- SpecialChar
+		-- sym"@character"         { }, -- Character
+		-- sym"@character.special" { }, -- SpecialChar
+		-- sym"@number"            { }, -- Number
+		-- sym"@boolean"           { }, -- Boolean
+		-- sym"@float"             { }, -- Float
+		sym("@function")({ fg = palette.blue }), -- Function
+		-- sym"@function.builtin"  { }, -- Special
+		sym("@function.macro")({ fg = palette.blue }), -- Macro
+		-- sym"@parameter"         { }, -- Identifier
+		-- sym"@method"            { }, -- Function
+		sym("@field")({ fg = palette.gray }), -- Field (object fields, etc.)
+		sym("@property")({ fg = palette.gray }), -- Property (object properties, etc.)
+		-- sym"@constructor"       { }, -- Special
+		-- sym"@conditional"       { }, -- Conditional
+		-- sym"@repeat"            { }, -- Repeat
+		-- sym"@label"             { }, -- Label
+		-- sym"@operator"          { }, -- Operator
+		sym("@keyword")({ fg = palette.gray }), -- Keyword (async, await, etc.)
+		-- sym"@exception"         { }, -- Exception
+		-- sym"@variable"          { }, -- Identifier
+		-- sym"@type"              { }, -- Type
+		-- sym"@type.definition"   { }, -- Typedef
+		sym("@storageclass")({ fg = palette.gray, gui = "bold" }), -- StorageClass (const, let, var)
+		-- sym"@structure"         { }, -- Structure
+		-- sym"@namespace"         { }, -- Identifier
+		-- sym"@include"           { }, -- Include
+		-- sym"@preproc"           { }, -- PreProc
+		-- sym"@debug"             { }, -- Debug
+		-- sym"@tag"               { }, -- Tag
+	}
+end)
+
+return theme
+
+-- vi:nowrap
